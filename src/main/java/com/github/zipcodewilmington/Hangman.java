beff3695fb981c742b9ba6f2d35bdd1f026ed988
Matt Kramer <matthew123003@gmail.com>
2024-03-01 07:27:48 -0500

    I started this

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
  */
 public class Hangman {
 
-    private 
-
+    private static final String[] words = {"cat", "dog", "six", "box", "big", "sit"};
+    private static final
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         System.out.println("Let's play word guess");
