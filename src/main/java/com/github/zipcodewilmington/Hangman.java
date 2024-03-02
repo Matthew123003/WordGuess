3b71ed67b135c2d04f2531450ec6a5d7ed5918bb
Matt Kramer <matthew123003@gmail.com>
2024-03-02 12:45:32 -0500

    Back to sort of working

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,13 +1,6 @@
-package com.github.zipcodewilmington;
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- * @author xt0fer
- * @version 1.0.0
- * @date 5/27/21 11:02 AM
- */
 public class Hangman {
     String[] gameWords = {"cat", "dog", "sit", "sun"};
 
@@ -15,33 +8,32 @@ public class Hangman {
     int randomIndex = random.nextInt(gameWords.length);
     String wordsToGuess = gameWords[randomIndex];// Pull words randomly from the array
 
-    char[] letters = wordsToGuess.toCharArray();
-    char inputChar = input.charAt(0);
-
-        for(int i = 0; i < letters.length; i++){
-        if(letters[i] == inputChar){
-            System.out.println();
-        }
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         System.out.println("Let's play hangman");
         System.out.println("Guess a letter");
-        String input = in.nextLine();
-        }
+        String input  = in.nextLine();
 
-        }
+        String[] gameWords = {"cat", "dog", "sit", "sun"};
 
+        Random random = new Random();
+        int randomIndex = random.nextInt(gameWords.length);
+        String wordsToGuess = gameWords[randomIndex];// Pull words randomly from the array
 
-        }
+        char[] letters = wordsToGuess.toCharArray();
+        char inputChar = input.charAt(0);
+        char[] letters = wordsToGuess.toCharArray();
+        char inputChar = input.charAt(0);
 
+        for(int i = 0; i < letters.length; i++){
+            if(letters[i] == inputChar){
+                if(letters[i] == inputChar){
+                    System.out.println();
+                }
 
-
-
-
-
-
-
-
-
-
+            }
+            public static void main(String[] args) {
+                Scanner in = new Scanner(System.in);
+                System.out.println("Let's play hangman");
+                System.out.println("Guess a letter");
+                String input = in.nextLine();
