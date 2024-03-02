7ca419d2fa587ae4fe922f0ae344d12e3c1a294e
Matt Kramer <matthew123003@gmail.com>
2024-03-02 12:49:30 -0500

    a little better but far from good

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -8,11 +8,11 @@ public class Hangman {
     int randomIndex = random.nextInt(gameWords.length);
     String wordsToGuess = gameWords[randomIndex];// Pull words randomly from the array
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Let's play hangman");
         System.out.println("Guess a letter");
-        String input  = in.nextLine();
+        String input = in.nextLine();
 
         String[] gameWords = {"cat", "dog", "sit", "sun"};
 
@@ -22,18 +22,19 @@ public class Hangman {
 
         char[] letters = wordsToGuess.toCharArray();
         char inputChar = input.charAt(0);
-        char[] letters = wordsToGuess.toCharArray();
-        char inputChar = input.charAt(0);
 
-        for(int i = 0; i < letters.length; i++){
-            if(letters[i] == inputChar){
-                if(letters[i] == inputChar){
-                    System.out.println();
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i] == inputChar) {
+                if (letters[i] == inputChar) {
+                    System.out.println(wordsToGuess);
                 }
 
             }
-            public static void main(String[] args) {
+        }
+    }
+}
+            /*public static void main(String[] args) {
                 Scanner in = new Scanner(System.in);
                 System.out.println("Let's play hangman");
                 System.out.println("Guess a letter");
-                String input = in.nextLine();
+                String input = in.nextLine();*/
