8342e124fc7f152eab540191c1c119f7f4bf9516
Matt Kramer <matthew123003@gmail.com>
2024-03-02 12:08:34 -0500

    Getting there i think

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -9,27 +9,25 @@ import java.util.Scanner;
  * @date 5/27/21 11:02 AM
  */
 public class Hangman {
+    String[] gameWords = {"cat", "dog", "sit", "sun"};
 
+    Random random = new Random();
+    int randomIndex = random.nextInt(gameWords.length);
+    String wordsToGuess = gameWords[randomIndex];// Pull words randomly from the array
 
-    public static void main(String[] args){
+    char[] letters = wordsToGuess.toCharArray();
+    char inputChar = input.charAt(0);
+
+        for(int i = 0; i < letters.length; i++){
+        if(letters[i] == inputChar){
+            System.out.println();
+        }
+
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Let's play hangman");
         System.out.println("Guess a letter");
-        String input  = in.nextLine();
-
-        String[] gameWords = {"cat", "dog", "sit", "sun"};
-
-       Random random = new Random();
-       int randomIndex = random.nextInt(gameWords.length);
-       String wordsToGuess = gameWords[randomIndex];// Pull words randomly from the array
-
-        char[] letters = wordsToGuess.toCharArray();
-        char inputChar = input.charAt(0);
-
-        for(int i = 0; i < letters.length; i++){
-            if(letters[i] == inputChar){
-
-            }
+        String input = in.nextLine();
         }
 
         }
